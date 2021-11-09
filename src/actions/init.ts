04ed539f78278b3aed906fa417ab7866697e7e11
Maciej Caderek <maciej.caderek@gmail.com>
2021-11-09 18:58:59 +0100

    Added option to pass language and year via arguments to the init command

--- src/actions/init.ts
+++ src/actions/init.ts
@@ -3,11 +3,14 @@ import benchmark from "../benchmark"
 import { save } from "../config"
 import calibrate from "./calibrate"
 
-const ask = (rl, question) =>
+const ask = (rl, question): Promise<string> =>
   new Promise((resolve) => rl.question(question, resolve))
 
 const init = async () => {
   console.log("Initializing")
+
+  const commandPos = process.argv.indexOf("init")
+  let [language, year] = process.argv.slice(commandPos + 1)
 
   const yearData = process.cwd().match(/20\d{2}/)
 
@@ -16,16 +19,18 @@ const init = async () => {
     output: process.stdout,
   })
 
-  const year = await ask(
-    rl,
-    `Year${yearData ? ` (default ${yearData[0]})` : ""}: `,
-  )
-  const language = await ask(rl, "Language: ")
+  if (year === undefined) {
+    year = await ask(rl, `Year${yearData ? ` (default ${yearData[0]})` : ""}: `)
+  }
+
+  if (language === undefined) {
+    language = await ask(rl, "Language: ")
+  }
 
   rl.close()
 
   save({
-    version: "1.0.0",
+    version: "2.0.0",
     year: (year ? year : yearData[0]) as string,
     language: (language || "unknown") as string,
     days: [],
